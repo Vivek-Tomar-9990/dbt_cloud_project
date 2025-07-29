fd1d7f229847c1d5677524fe34c639f576f1f0e4
Vivek Tomar <pukniyepsi@gufum.com>
2025-07-29 18:08:46 +0000

    back all files

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,4 +1,8 @@
-{{ config(materialized='view') }}
+{{ config(
+    materialized='view',
+    pre_hook="insert into audit_db.audit_sch.dbt_audits(model_name, status) values ('{{this.name}}', 'Start'); commit;",
+    post_hook="insert into audit_db.audit_sch.dbt_audits(model_name, status) values ('{{this.name}}', 'End'); commit;"
+    ) }}
 SELECT
     id AS customer_id,
      upper(email) AS email,
